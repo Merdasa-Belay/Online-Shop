b852fe0c4f21f1e6bc16b70dfcae4d3b8d988755
Merdasa-Belay <merdasabelay@gmail.com>
2024-10-29 05:44:44 +0300

    update package

---
+++ src/assets/assets.js
@@ -0,0 +1,10 @@
+import image_1 from './image_1.jpg'
+import image_2 from './image_2.jpg'
+import image_3 from './image_3.jpg'
+
+
+export const assets = {
+    image_1,
+    image_2,
+    image_3,
+    }
