d9e7dc67801f8e080105287b39540e8d61d921d7
Merdasa-Belay <merdasabelay@gmail.com>
2024-12-22 12:03:36 +0300

    update

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,10 +1,25 @@
 import image_1 from './image_1.jpg'
 import image_2 from './image_2.jpg'
 import image_3 from './image_3.jpg'
+import koket_logo from './koket_logo.png'
+import yellow_sofa from './yellow_sofa.jpg'
 
 
 export const assets = {
     image_1,
     image_2,
     image_3,
+    koket_logo,
+    yellow_sofa
     }
+
+
+    export const products = [
+        {
+            _id : "aaa",
+            name:"woman round nake cotton top",
+            description: "A light weight, usually knitted, pullover shirt, close fitting and with",
+            price:100,
+            
+        }
+    ]
